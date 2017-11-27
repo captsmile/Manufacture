796555c56776d4c496e28f1dce476a0b8f00bc88
Vitalii <flipsun@gmail.com>
2017-11-27 17:53:01 +0200

    implemented authentification service across UserDetailsService with dropdown menu

--- src/main/java/com/ed/manufacture/ManufactureApplication.java
+++ src/main/java/com/ed/manufacture/ManufactureApplication.java
@@ -3,6 +3,12 @@ package com.ed.manufacture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//TODO designe login form
+//TODO create registration
+//TODO sockets
+//TODO graph and charts d3js
+//TODO test
+
 @SpringBootApplication
 public class ManufactureApplication {
 
